174984ab526f1e2c5192d8dffd1288fcf8bb9ebb
dannysun725 <danny.sun@me.com>
2015-02-16 00:36:35 -0600

    corrected to east-1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     :provider               => 'AWS',
     :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"], # THING 1
     :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"], # THING 2
-    :region                 => 'us-west-2'
+    :region                 => 'us-east-1'
   }
-  
+
   config.fog_directory  = ENV["AWS_BUCKET"] # THING 3
   config.fog_public     = true
   config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
